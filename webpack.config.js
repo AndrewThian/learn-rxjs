7191bc61c39c0ad617d71efbaade6415886dea8e
AndrewThian <andrewbobbythian@gmail.com>
2019-01-29 10:11:29 +0800

    typo in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/code.ts',
-    devTool: 'inline-source-map',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
